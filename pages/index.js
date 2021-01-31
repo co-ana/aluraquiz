cdc83ad849828171bc77116509c3c8d16f4c0251
Ana Có <ana.cbe14@gmail.com>
2021-01-30 23:08:32 -0300

    initial

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,107 @@
 import styled from 'styled-components'
+import db from '../db.json'
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `
 
+const BackgroundImage = styled.div`
+  background-image: url(${db.bg});
+  background-position: center;
+  background-size: cover;
+  flex: 1;
+`
+
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`
+
+const Widget = styled.div`
+  margin-top: 24px;
+  margin-bottom: 24px;
+  border: 1px solid  ${({ theme }) => theme.colors.primary};
+  background-color: ${({ theme }) => theme.colors.mainBg};
+  border-radius: ${({ theme }) => theme.borderRadius};
+  overflow: hidden;
+
+  h1, h2, h3 {
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 1;
+    margin-bottom: 0;
+  }
+
+  p {
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1;
+  }
+`
+
+Widget.Header = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 18px 32px;
+  background-color: ${({ theme }) => theme.colors.primary};
+
+  * {
+    margin: 0;
+  }
+`
+
+Widget.Content = styled.div`
+  padding: 24px 32px 32px 32px;
+  & > *:first-child {
+    margin-top: 0;
+  }
+  & > *:last-child {
+    margin-bottom: 0;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+`
+
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <BackgroundImage>
+      <QuizContainer>
+        <h1>Quiz da galera..</h1>
+        <Widget>
+          <Widget.Header>
+            <h2>Widget 1</h2>
+          </Widget.Header>
+          <Widget.Content>
+            <p>idjfosifjsf</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Header>
+            <h2>Widget 1</h2>
+          </Widget.Header>
+          <Widget.Content>
+            <p>idjfosifjsf</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Header>
+            <h2>Widget 1</h2>
+          </Widget.Header>
+          <Widget.Content>
+            <p>idjfosifjsf</p>
+          </Widget.Content>
+        </Widget>
+      </QuizContainer>
+    </BackgroundImage>
+  )
 }
